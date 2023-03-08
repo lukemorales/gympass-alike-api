791f86be2a378c710c7882066a5f87e64a8c661b
Luke Morales <lukemorales@live.com>
2023-03-07 21:42:49 -0300

    Setup environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { app } from './app';
+import { ENV } from './shared/env';
 
 void app
-  .listen({ host: '0.0.0.0', port: 3333 })
-  .then(() => console.log('🚀 Server running...'));
+  .listen({ host: '0.0.0.0', port: ENV.PORT })
+  .then(() => console.log(`🚀 Server running on port ${ENV.PORT}`));
---
+++ src/shared/env.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { z } from 'zod';
+import { fromZodError } from 'zod-validation-error';
+
+const schema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  PORT: z.coerce.number().default(3333),
+});
+
+const result = schema.safeParse(process.env);
+
+if (!result.success) {
+  throw fromZodError(result.error);
+}
+
+export const ENV = result.data;
