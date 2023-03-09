954b3674d9f9d37685754bba2102293a9cb219f2
Luke Morales <lukemorales@live.com>
2023-03-08 23:49:58 -0300

    Validate `CheckIn` distance from `Gym`

--- src/features/check-ins/create-checkin.service.spec.ts
+++ src/features/check-ins/create-checkin.service.spec.ts
@@ -1,20 +1,24 @@
 import assert from 'assert';
+import { Decimal } from '@prisma/client/runtime/library';
 
 import { Clock } from '@features/clock';
 import { E } from '@shared/effect';
+import { GymsInMemoryRepository } from '@features/gyms';
 
 import { CreateCheckInService } from './create-checkin.service';
 import { CheckInsInMemoryRepository } from './repositories';
 
 describe('CreateCheckInService', () => {
   let clock: Clock;
-  let usersRepository: CheckInsInMemoryRepository;
+  let checkInsRepository: CheckInsInMemoryRepository;
+  let gymsRepository: GymsInMemoryRepository;
   let sut: CreateCheckInService;
 
   beforeEach(() => {
     clock = new Clock();
-    usersRepository = new CheckInsInMemoryRepository();
-    sut = new CreateCheckInService(clock, usersRepository);
+    checkInsRepository = new CheckInsInMemoryRepository();
+    gymsRepository = new GymsInMemoryRepository();
+    sut = new CreateCheckInService(clock, checkInsRepository, gymsRepository);
 
     vi.useFakeTimers();
   });
@@ -24,26 +28,103 @@ describe('CreateCheckInService', () => {
   });
 
   describe('execute', () => {
-    it('does not create a check-in if a previous check-in exists for the same day in the same gym', async () => {
-      vi.setSystemTime(new Date(2023, 2, 10, 8, 0, 0, 0));
+    const performSetup = async () => {
+      gymsRepository.repository.push(
+        {
+          id: 'gym_01',
+          name: 'Maromba Gym',
+          phone: null,
+          description: '',
+          latitude: new Decimal(0.987654321),
+          longitude: new Decimal(-0.987654321),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 'gym_02',
+          name: 'Super Maromba Gym',
+          phone: null,
+          description: '',
+          latitude: new Decimal(0.123456789),
+          longitude: new Decimal(-0.123456789),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      );
+    };
 
-      await sut.execute({
-        userId: 'user_01',
-        gymId: 'gym_01',
-      });
+    beforeEach(async () => {
+      await performSetup();
+    });
 
+    it('fails with a "ResourceNotFound" error if the gym does not exist', async () => {
       const result = await sut.execute({
         userId: 'user_01',
-        gymId: 'gym_01',
+        gymId: 'gym_invalid_id',
+        coords: {
+          lat: 0.987654321,
+          long: -0.987654321,
+        },
       });
 
       assert.ok(E.isLeft(result));
 
       expect(result.left).toMatchInlineSnapshot(`
-        DuplicatedCheckInNotAllowed {
+        ResourceNotFound {
+          "resourceId": "gym_invalid_id",
+          "tag": "ResourceNotFound",
+        }
+      `);
+    });
+
+    it('fails with a "DuplicateCheckInNotAllowed" error if a previous check-in exists for the same day in the same gym', async () => {
+      vi.setSystemTime(new Date(2023, 2, 10, 8, 0, 0, 0));
+
+      await sut.execute({
+        userId: 'user_01',
+        gymId: 'gym_01',
+        coords: {
+          lat: 0.987654321,
+          long: -0.987654321,
+        },
+      });
+
+      const result = await sut.execute({
+        userId: 'user_01',
+        gymId: 'gym_01',
+        coords: {
+          lat: 0.987654321,
+          long: -0.987654321,
+        },
+      });
+
+      assert.ok(E.isLeft(result));
+
+      expect(result.left).toMatchInlineSnapshot(`
+        DuplicateCheckInNotAllowed {
           "date": 2023-03-10T11:00:00.000Z,
           "gymId": "gym_01",
-          "tag": "DuplicatedCheckInNotAllowed",
+          "tag": "DuplicateCheckInNotAllowed",
+        }
+      `);
+    });
+
+    it('fails with a "NotOnLocation" error if distance from gym is greater than allowed', async () => {
+      const result = await sut.execute({
+        userId: 'user_01',
+        gymId: 'gym_01',
+        coords: {
+          lat: 0.917257389,
+          long: -0.987654321,
+        },
+      });
+
+      assert.ok(E.isLeft(result));
+
+      expect(result.left).toMatchInlineSnapshot(`
+        NotOnLocation {
+          "distance": 7.827405088086411,
+          "tag": "NotOnLocation",
         }
       `);
     });
@@ -52,6 +133,10 @@ describe('CreateCheckInService', () => {
       const result = await sut.execute({
         userId: 'user_01',
         gymId: 'gym_01',
+        coords: {
+          lat: 0.987654321,
+          long: -0.987654321,
+        },
       });
 
       assert.ok(E.isRight(result));
@@ -62,6 +147,38 @@ describe('CreateCheckInService', () => {
         id: expect.any(String),
         user_id: 'user_01',
         gym_id: 'gym_01',
+      });
+    });
+
+    it('creates another check-in in the same day on a different gym', async () => {
+      vi.setSystemTime(new Date(2023, 2, 10, 8, 0, 0, 0));
+
+      await sut.execute({
+        userId: 'user_01',
+        gymId: 'gym_01',
+        coords: {
+          lat: 0.987654321,
+          long: -0.987654321,
+        },
+      });
+
+      const result = await sut.execute({
+        userId: 'user_01',
+        gymId: 'gym_02',
+        coords: {
+          lat: 0.123456789,
+          long: -0.123456789,
+        },
+      });
+
+      assert.ok(E.isRight(result));
+
+      const { checkIn } = result.right;
+
+      expect(checkIn).toMatchObject({
+        id: expect.any(String),
+        user_id: 'user_01',
+        gym_id: 'gym_02',
       });
     });
   });
--- src/features/check-ins/create-checkin.service.ts
+++ src/features/check-ins/create-checkin.service.ts
@@ -3,22 +3,40 @@ import { type CheckIn } from '@prisma/client';
 
 import { E, O } from '@shared/effect';
 import { type Clock } from '@features/clock';
+import { type GymsRepository } from '@features/gyms';
+import { ResourceNotFound } from '@shared/failures';
 
 import { type CheckInsRepository } from './repositories';
+import { getDistanceBetweenCoordinates } from './get-distance-between-coordinates';
+
+const MAX_DISTANCE_FROM_GYM_IN_KILOMETERS = 0.1;
 
 export const createCheckInPayload = z.object({
   userId: z.string().min(1),
   gymId: z.string().min(1),
+  coords: z.object({
+    lat: z.number(),
+    long: z.number(),
+  }),
 });
 
 type CreateCheckInPayload = z.infer<typeof createCheckInPayload>;
 
-type CheckInCreationError = DuplicatedCheckInNotAllowed;
+type CheckInCreationError =
+  | DuplicateCheckInNotAllowed
+  | ResourceNotFound
+  | NotOnLocation;
 
-class DuplicatedCheckInNotAllowed {
-  readonly tag = 'DuplicatedCheckInNotAllowed';
+class DuplicateCheckInNotAllowed {
+  readonly tag = 'DuplicateCheckInNotAllowed';
 
   constructor(readonly date: Date, readonly gymId: string) {}
+}
+
+class NotOnLocation {
+  readonly tag = 'NotOnLocation';
+
+  constructor(readonly distance: number) {}
 }
 
 class CheckInCreated {
@@ -31,14 +49,22 @@ export class CreateCheckInService {
   constructor(
     private readonly clock: Clock,
     private readonly checkInsRepository: CheckInsRepository,
+    private readonly gymsRepository: GymsRepository,
   ) {}
 
   async execute({
     userId,
     gymId,
+    coords,
   }: CreateCheckInPayload): Promise<
     E.Either<CheckInCreationError, CheckInCreated>
   > {
+    const maybeGym = await this.gymsRepository.findById(gymId);
+
+    if (O.isNone(maybeGym)) {
+      return E.left(new ResourceNotFound(gymId));
+    }
+
     const date = this.clock.now;
 
     const maybeCheckIn = await this.checkInsRepository.findByMembershipAndDate({
@@ -48,7 +74,18 @@ export class CreateCheckInService {
     });
 
     if (O.isSome(maybeCheckIn)) {
-      return E.left(new DuplicatedCheckInNotAllowed(date, gymId));
+      return E.left(new DuplicateCheckInNotAllowed(date, gymId));
+    }
+
+    const gym = maybeGym.value;
+
+    const distanceFromGym = getDistanceBetweenCoordinates(coords, {
+      lat: gym.latitude.toNumber(),
+      long: gym.longitude.toNumber(),
+    });
+
+    if (distanceFromGym > MAX_DISTANCE_FROM_GYM_IN_KILOMETERS) {
+      return E.left(new NotOnLocation(distanceFromGym));
     }
 
     const checkIn = await this.checkInsRepository.create({ userId, gymId });
