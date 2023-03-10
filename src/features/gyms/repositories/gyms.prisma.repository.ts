2cac5f197a39e5625a47db6cd834520afb208724
Luke Morales <lukemorales@live.com>
2023-03-09 23:57:11 -0300

    Implement missing `create` method in `GymsPrismaRepository`

--- src/features/gyms/repositories/gyms.prisma.repository.ts
+++ src/features/gyms/repositories/gyms.prisma.repository.ts
@@ -1,15 +1,27 @@
 import { type PrismaClient } from '@prisma/client';
+import { ulid } from 'ulid';
 
 import { prisma } from '@shared/prisma';
 import { O, pipe } from '@shared/effect';
 
-import { type GymsRepository } from './gyms.repository';
+import { type CreateGymOptions, type GymsRepository } from './gyms.repository';
 
 export class GymsPrismaRepository implements GymsRepository {
   private readonly repository: PrismaClient['gym'];
 
   constructor() {
     this.repository = prisma.gym;
+  }
+
+  async create(options: CreateGymOptions) {
+    const gym = await this.repository.create({
+      data: {
+        ...options,
+        id: ulid(),
+      },
+    });
+
+    return gym;
   }
 
   async findById(id: string) {
