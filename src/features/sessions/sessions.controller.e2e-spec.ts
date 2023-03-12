abb3b28c70b936699304a894f70205601846c80f
Luke Morales <lukemorales@live.com>
2023-03-12 17:14:43 -0300

    Write e2e tests for session creation

---
+++ src/features/sessions/sessions.controller.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { app } from 'src/app';
+import request from 'supertest';
+
+describe('SessionsController | e2e', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('POST /', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'john@doe.com',
+        password: '123456',
+      })
+      .expect(201);
+
+    const response = await request(app.server)
+      .post('/sessions')
+      .send({
+        email: 'john@doe.com',
+        password: '123456',
+      })
+      .expect(200);
+
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
+});
