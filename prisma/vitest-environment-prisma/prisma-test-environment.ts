b8f1ec3784018412a3cd59d38b300666c5d768a7
Luke Morales <lukemorales@live.com>
2023-03-12 16:55:26 -0300

    Isolate e2e databases

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,17 +1,44 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
+import 'dotenv/config';
+
 import { type Environment } from 'vitest';
+import { ulid } from 'ulid';
+import { execSync } from 'node:child_process';
+import { PrismaClient } from '@prisma/client';
+
+function generateDatabaseUrl(schema: string) {
+  const databaseUrl = process.env.DATABASE_URL;
+
+  if (!databaseUrl) {
+    throw new Error('DATABASE_URL missing in environment file');
+  }
+
+  const url = new URL(databaseUrl);
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+}
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    // eslint-disable-next-line no-console
-    console.log('setup');
+    const schema = ulid();
+
+    const databaseUrl = generateDatabaseUrl(schema);
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync('npx prisma migrate deploy');
 
     return {
-      teardown(_global) {
-        // eslint-disable-next-line no-console
-        console.log('teardown');
+      async teardown(_global) {
+        const prisma = new PrismaClient();
+
+        await prisma.$executeRawUnsafe(`
+          DROP SCHEMA IF EXISTS "${schema}" CASCADE
+        `);
+
+        await prisma.$disconnect();
       },
     };
   },
