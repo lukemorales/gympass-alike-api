48bbd28abdd627512baa2e999a172c00b623ed43
Luke Morales <lukemorales@live.com>
2023-03-12 01:24:10 -0300

    Improve global `ZodError` handling for invalid entity ids

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,20 @@ routes.forEach(
 
 app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
+    const hasInvalidUlidError = error.issues.some(
+      (issue) => issue.code === 'custom' && error.message.includes('ULID'),
+    );
+
+    const hasInvalidEntityIdError = error.issues.some(
+      (issue) =>
+        issue.code === 'invalid_string' &&
+        error.message.includes('Invalid entity id'),
+    );
+
+    if (hasInvalidUlidError || hasInvalidEntityIdError) {
+      return reply.status(404).send({ message: 'Resource not found' });
+    }
+
     return reply.status(400).send(fromZodError(error));
   }
 
